6f89e4d62243f5027cdaa1af1cab09d1ce281aba
tonybrianmwenda <tonybrianmwenda@gmail.com>
2024-03-24 21:07:37 +0300

    complete lab

---
+++ speed.js
@@ -0,0 +1,12 @@
+let carSpeed = 80(){
+    console.log(carSpeed);
+}
+{
+    if carSpeed < 70
+    {
+        console.log("OK");
+    }
+    else if carSpeed > 70 {
+        console.log("demerit points");
+    }
+}
